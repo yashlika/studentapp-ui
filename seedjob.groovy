bc2e12e6f2f1a93384562398ae2ac30b206355e7
saif <saif@gmail.com>
2022-04-08 14:55:20 +0530

    --- Auto-Git Commit ---

---
+++ seedjob.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages {
+        stage('pull') {
+            steps {
+                git 'https://github.com/Ashmirasheikh/demo.git'
+            }
+        }
+        stage('test') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+    }
+}
