2ea1183a884197b08efcd0b41d188e23725e7c8f
saif <saif@gmail.com>
2022-04-16 16:04:18 +0530

    hello

---
+++ sonar.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+        stage('pull') {
+            steps {
+                git 'https://github.com/Ashmirasheikh/studentapp-ui.git'
+            }
+        }
+         stage('build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage('test') {
+            steps { withSonarQubeEnv ('sonar') {
+                sh 'mvn sonar:sonar'
+                }
+            }
+        }
+    }
+}
