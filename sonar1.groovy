a4bc10989b8cdb9b1f44f7f116b337768ae86f3d
saif <saif@gmail.com>
2022-04-16 19:25:35 +0530

    sonar1

---
+++ sonar1.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any 
+    stages {
+        stage('pull') { 
+            steps {
+                // 
+            }
+        }
+        stage('build') { 
+            steps {
+                // 
+            }
+        }
+        stage('test') { 
+            steps {
+                // 
+            }
+        }
+    }
+}
